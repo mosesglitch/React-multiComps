da68bdb7e840d6ec9ca0501e285a1647f4eab5b1
mosesglitch <mosesgachuru15@gmail.com>
2023-03-22 19:57:24 +0300

    add switch in reducer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -2,8 +2,36 @@ import { useState, useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
 
+const INCREMENT_COUNT = "increment";
+const SET_VALUE_TO_ADD = "change_value_to_add";
+const DECREMENT_COUNT = "decerement";
 const reducer = (state, action) => {
-  return { ...state, count: state.count + 1 };
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return { ...state, count: state.count + 1 };
+
+    case SET_VALUE_TO_ADD:
+      return {
+        ...state,
+        valueToAdd: action.payload,
+      };
+    case DECREMENT_COUNT:
+      return { ...state, count: state.count - 1 };
+
+    default:
+      throw new Error("unexpected action type:" + action.type);
+    // return state;
+  }
+  // if (action.type === INCREMENT_COUNT) {
+  //   return { ...state, count: state.count + 1 };
+  // }
+  // if (action.type === SET_VALUE_TO_ADD) {
+  //   return {
+  //     ...state,
+  //     valueToAdd: action.payload,
+  //   };
+  // }
+  // return state;
 };
 
 function CounterPage({ initialCount }) {
@@ -13,15 +41,25 @@ function CounterPage({ initialCount }) {
     count: initialCount,
     valueToAdd: 0,
   });
+  console.log(state);
   const increment = () => {
-    dispatch();
+    dispatch({
+      type: INCREMENT_COUNT,
+    });
     // setCount(count + 1);
   };
   const decrement = () => {
     // setCount(count - 1);
+    dispatch({
+      type: INCREMENT_COUNT,
+    });
   };
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
+    dispatch({
+      type: SET_VALUE_TO_ADD,
+      payload: value,
+    });
     // setValueToAdd(value);
   };
   const handleSubmit = (event) => {
